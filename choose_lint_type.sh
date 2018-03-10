02765f90c6d029333dbbf127049999585e0c50c9
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2018-03-10 22:19:04 +0900

    first commint

---
+++ choose_lint_type.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 引数にファイルを受け取る
+fpath=${1:?}
+
+# 拡張子を抽出
+ftype="${fpath##*.}"
+
+# 拡張子が無い場合は空にする
+if [ "${fpath}" = "${ftype}" ]; then
+    ftype=""
+fi
+
+# 拡張子ごとに lint を実行する
+case "$ftype" in
+    "yml" | "yaml" ) ansible-lint -c lint-rules/rules/ansible/.ansible-lint ${fpath}  ;;
+    * ) echo No matching for lint test ;;
+esac
