cd4e01c58ca5bb3300a6a415d82681c9f00cbca2
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2018-03-11 02:08:56 +0900

    fixed bug

--- choose_lint_type.sh
+++ choose_lint_type.sh
@@ -24,13 +24,15 @@ function exec-lint () {
 
     # 拡張子ごとに lint を実行する
     case "$ftype" in
-        "yml" | "yaml" ) ansible-lint -c lint-rules/rules/ansible/.ansible-lint ${fpath}  ;;
-        * ) echo ${fpath:?}: There is no matching lint test for this file type. ;;
+        "yml" | "yaml" )
+            ansible-lint -c lint-rules/rules/ansible/.ansible-lint ${fpath}
+            if [ $? != 0 ]; then exit $?; fi
+            ;;
+        * )
+            echo ${fpath:?}: There is no matching lint test for this file type.
+            ;;
     esac
 
-    if [ $? != 0 ]; then
-        exit $?
-    fi
 }
 
 # lint 実行
