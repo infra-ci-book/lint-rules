170dac4d2352fcae011f9253d434314dc864f5fb
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2018-03-10 22:40:27 +0900

    test

--- choose_lint_type.sh
+++ choose_lint_type.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-# 引数にファイルを受け取る
-fpath=${1:?}
+# 変更ファイルを抽出する
+flist=`git diff --name-only --diff-filter=ACM origin/master...HEAD`
+echo ${flist}
 
-# 拡張子を抽出
-ftype="${fpath##*.}"
-
-# 拡張子が無い場合は空にする
-if [ "${fpath}" = "${ftype}" ]; then
-    ftype=""
+# 変更ファイルが0の時は終了
+if [ "${flist}" = "" ]; then
+    echo There is no file in this commint.
+    exit 0
 fi
 
-# 拡張子ごとに lint を実行する
-case "$ftype" in
-    "yml" | "yaml" ) ansible-lint -c lint-rules/rules/ansible/.ansible-lint ${fpath}  ;;
-    * ) echo ${ftype:?}: There is no matching lint test for this file type. ;;
-esac
+# lint 実行
+for i in ${flist:?}
+do
+    exec-lint ${i}
+done
+
+
+function exec-lint () {
+    # 引数にファイルを受け取る
+    fpath=${1:?}
+
+    # 拡張子を抽出
+    ftype="${fpath##*.}"
+
+    # 拡張子が無い場合は空にする
+    if [ "${fpath}" = "${ftype}" ]; then
+        ftype=""
+    fi
+
+    # 拡張子ごとに lint を実行する
+    case "$ftype" in
+        "yml" | "yaml" ) ansible-lint -c lint-rules/rules/ansible/.ansible-lint ${fpath}  ;;
+        * ) echo ${ftype:?}: There is no matching lint test for this file type. ;;
+    esac
+}
